c05188732bfb10cfa2abd31d4e87d0a1f4e51b77
flvsantos15 <flvsantos300@gmail.com>
2025-04-09 22:42:25 -0300

    chore: update contact admin file

--- agenda-django/contact/admin.py
+++ agenda-django/contact/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
 from contact import models
+from django.contrib import admin
+
 
 # Register your models here.
 @admin.register(models.Contact)
@@ -7,11 +8,11 @@ class ContactAdmin(admin.ModelAdmin):
   list_display = 'id', 'first_name', 'last_name', 'phone',
   ordering = 'id', # Se eu precisar que seja decrescente é só colocar o - (-id)
   # list_filter = 'created_date' = lista de filtros, posso adicionar varios so separando por virgula
-  search_fields = 'id', 'first_name', 'last_name',
-  list_per_page = 10
-  list_max_show_all = 200
-  list_editable = 'first_name', 'last_name',
-  list_display_links = 'id', 'phone',
+  search_fields = 'id', 'first_name', 'last_name', # adiciono o campo de busca
+  list_per_page = 10 # quantidade de registros por pagina
+  list_max_show_all = 200 # quantidade maximo de registros para ver todos
+  list_editable = 'first_name', 'last_name', # campos que podem ser editados
+  list_display_links = 'id', 'phone', # campos que podem ser clicados
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
