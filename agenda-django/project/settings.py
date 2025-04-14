0333a089e5cb077b787b07f177b164e752513bc6
flvsantos15 <flvsantos300@gmail.com>
2025-04-14 09:16:14 -0300

    chore: add create user script

--- agenda-django/project/settings.py
+++ agenda-django/project/settings.py
@@ -130,3 +130,8 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+  from project.local_settings import *
+except ImportError:
+  ...
