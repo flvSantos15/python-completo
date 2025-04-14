2197db14a3dd88bdda1eda07f3cb8c9fb107d064
flvsantos15 <flvsantos300@gmail.com>
2025-04-13 22:38:48 -0300

    chore: update models file

--- agenda-django/contact/models.py
+++ agenda-django/contact/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -10,6 +11,9 @@ from django.utils import timezone
 # owner (foreign key)
 
 class Category(models.Model):
+  class Meta:
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
   name = models.CharField(max_length=50)
 
   def __str__(self) -> str:
@@ -26,6 +30,11 @@ class Contact(models.Model):
   picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
   category = models.ForeignKey(
     Category,
+    on_delete=models.SET_NULL,
+    blank=True, null=True
+  )
+  owner = models.ForeignKey(
+    User,
     on_delete=models.SET_NULL,
     blank=True, null=True
   )
