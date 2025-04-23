be5fce40b1c25efffb0144715340d92f29141a5e
flvsantos15 <flvsantos300@gmail.com>
2025-04-23 09:45:59 -0300

    chore: add contact table

--- agenda-django/utils/create_contact.py
+++ agenda-django/utils/create_contact.py
@@ -10,52 +10,50 @@ from django.conf import settings
 DJANGO_BASE_DIR = Path(__file__).parent.parent
 NUMBER_OF_OBJECTS = 1000
 
-sys.path.append(DJANGO_BASE_DIR)
+sys.path.append(str(DJANGO_BASE_DIR))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
-settings.TIME_ZONE = 'UTC'
+settings.USE_TZ = False
 
 django.setup()
 
 if __name__ == '__main__':
-  import faker
-  from contact.models import Category, Contact
+    import faker
 
-  Contact.objects.all().delete()
-  Category.objects.all().delete()
+    from contact.models import Category, Contact
 
-  fake = faker.Faker('pt_BR')
-  categories = ['Amigos', 'Família', 'Trabalho', 'Conhecidos']
+    Contact.objects.all().delete()
+    Category.objects.all().delete()
 
-  django_categories = [Category(name=category) for category in categories]
+    fake = faker.Faker('pt_BR')
+    categories = ['Amigos', 'Família', 'Conhecidos']
 
-  for category in django_categories:
-    category.save()
+    django_categories = [Category(name=name) for name in categories]
 
-  django_contacts = []
+    for category in django_categories:
+        category.save()
 
-  for _ in range(NUMBER_OF_OBJECTS):
-    profile = fake.profile()
-    email = profile['mail']
-    first_name, last_name = profile['name'].split(' ', 1)
-    phone = f'+55 (98) {fake.random_number(digits=9)}'
-    created_at = datetime - fake.date_this_year()
-    description = fake.text(max_nb_chars=100)
-    category = choice(django_categories)
+    django_contacts = []
 
-    django_contacts.append(Contact(
-      first_name=first_name,
-      last_name=last_name,
-      email=email,
-      phone=phone,
-      description=description,
-      category=category,
-      created_at=created_at
-    ))
+    for _ in range(NUMBER_OF_OBJECTS):
+        profile = fake.profile()
+        email = profile['mail']
+        first_name, last_name = profile['name'].split(' ', 1)
+        phone = fake.phone_number()
+        created_date: datetime = fake.date_this_year()
+        description = fake.text(max_nb_chars=100)
+        category = choice(django_categories)
 
-  if len(django_contacts) > 0:
-    Contact.objects.bulk_create(django_contacts)
-    print(f'Created {len(django_contacts)} contacts')
-    
+        django_contacts.append(
+            Contact(
+                first_name=first_name,
+                last_name=last_name,
+                phone=phone,
+                email=email,
+                created_date=created_date,
+                description=description,
+                category=category,
+            )
+        )
 
-  
-
+    if len(django_contacts) > 0:
+        Contact.objects.bulk_create(django_contacts)
