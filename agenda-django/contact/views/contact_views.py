8b1e0616a9b3bdf0b48c19e99d22bf8114f9e90c
flvsantos15 <flvsantos300@gmail.com>
2025-04-29 18:59:08 -0300

    chore: add contact view

--- agenda-django/contact/views/contact_views.py
+++ agenda-django/contact/views/contact_views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from contact.models import Contact
 
 # Create your views here.
 def index(request):
   contacts = Contact.objects \
-    .filter(show=True).order_by('-1')
+        .filter(show=True)\
+        .order_by('-id')
 
   context = {
     'contacts': contacts,
@@ -13,5 +15,21 @@ def index(request):
   return render(
     request,
     'contact/index.html',
+    context
+  )
+
+def contact(request, contact_id):
+  # single_contact = Contact.objects.filter(pk=contact_id).first()
+  single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+  # single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id))
+  # single_contact = get_object_or_404(Contact.objects, pk=contact_id)
+
+  context = {
+    'contact': single_contact,
+  }
+
+  return render(
+    request,
+    'contact/contact.html',
     context
   )
