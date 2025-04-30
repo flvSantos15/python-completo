dd72ab3a6f66d3aa47df972c4a609ea998f52154
flvsantos15 <flvsantos300@gmail.com>
2025-04-30 10:54:03 -0300

    chore: add updates

--- agenda-django/contact/views/contact_views.py
+++ agenda-django/contact/views/contact_views.py
@@ -10,6 +10,7 @@ def index(request):
 
   context = {
     'contacts': contacts,
+    'site_title': 'Contatos',
   }
 
   return render(
@@ -23,9 +24,12 @@ def contact(request, contact_id):
   single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
   # single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id))
   # single_contact = get_object_or_404(Contact.objects, pk=contact_id)
+  
+  site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
   context = {
     'contact': single_contact,
+    'site_title': site_title,
   }
 
   return render(
