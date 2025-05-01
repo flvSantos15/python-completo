37fe7095c02686174c24232024df7e62a093f6b2
flvsantos15 <flvsantos300@gmail.com>
2025-05-01 13:58:32 -0300

    chore: add search functionallity

--- agenda-django/contact/views/contact_views.py
+++ agenda-django/contact/views/contact_views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from contact.models import Contact
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -37,3 +38,32 @@ def contact(request, contact_id):
     'contact/contact.html',
     context
   )
+
+def search(request):
+  search_value = request.GET.get('q', '').strip()
+
+  if search_value == '':
+    return redirect('contact:index')
+
+  contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+          Q(first_name__icontains=search_value) |
+          Q(last_name__icontains=search_value) |
+          Q(email__icontains=search_value) |
+          Q(phone__icontains=search_value)
+        ) \
+        .order_by('-id')
+
+  context = {
+    'contacts': contacts,
+    'site_title': 'Search -',
+    'search_value': search_value,
+  }
+
+  return render(
+    request,
+    'contact/index.html',
+    context
+  )
+
