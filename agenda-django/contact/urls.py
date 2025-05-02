ec1707e4111060d493c0ef353b5fb7343a56bd50
flvsantos15 <flvsantos300@gmail.com>
2025-05-02 19:22:16 -0300

    chore: add url pattern

--- agenda-django/contact/urls.py
+++ agenda-django/contact/urls.py
@@ -6,5 +6,9 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
-    path('<int:contact_id>/', views.contact, name='contact'),
+    
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
