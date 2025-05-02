79e78a1cca83326c69ddd2454329fdb8dd66120b
flvsantos15 <flvsantos300@gmail.com>
2025-05-02 19:12:38 -0300

    chore: add pagination

--- agenda-django/contact/views/contact_views.py
+++ agenda-django/contact/views/contact_views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
   contacts = Contact.objects \
         .filter(show=True)\
         .order_by('-id')
+  
+  paginator = Paginator(contacts, 10)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
 
   context = {
-    'contacts': contacts,
+    'page_obj': page_obj,
     'site_title': 'Contatos',
   }
 
@@ -55,8 +59,12 @@ def search(request):
         ) \
         .order_by('-id')
 
+  paginator = Paginator(contacts, 10)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+
   context = {
-    'contacts': contacts,
+    'page_obj': page_obj,
     'site_title': 'Search -',
     'search_value': search_value,
   }
