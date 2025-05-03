9fa1bca5c32b71525fce39a0437f21506698a801
flvsantos15 <flvsantos300@gmail.com>
2025-05-02 21:53:25 -0300

    chore: add pagination

--- agenda-django/contact/urls.py
+++ agenda-django/contact/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
-    path('contact/create/', views.contact, name='contact'),
-    path('contact/<int:contact_id>/update/', views.contact, name='contact'),
-    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
