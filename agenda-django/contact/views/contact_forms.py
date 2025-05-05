327892ac6e3b7220f2d847c2271bd66495103cd3
flvsantos15 <flvsantos300@gmail.com>
2025-05-05 13:18:48 -0300

    feat: add contact form

--- agenda-django/contact/views/contact_forms.py
+++ agenda-django/contact/views/contact_forms.py
@@ -2,14 +2,32 @@ from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django import forms
+
+class ContacForm(forms.ModelForm):
+  class Meta:
+    model = Contact
+    fields = (
+      'first_name',
+      'last_name',
+      'phone'
+    )
 
 # Create your views here.
 def create(request):
   if request.method == 'POST':
-    pass
+    context = {
+      'form': ContacForm(request.POST),
+    }
+
+    return render(
+      request,
+      'contact/create.html',
+      context
+    )
 
   context = {
-
+    'form': ContacForm(),
   }
 
   return render(
