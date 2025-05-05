8606bd3afea944215bb66ff8d9db5616460a082c
flvsantos15 <flvsantos300@gmail.com>
2025-05-05 13:31:35 -0300

    chore: move contact form to forms.py

--- agenda-django/contact/views/contact_forms.py
+++ agenda-django/contact/views/contact_forms.py
@@ -1,17 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from contact.models import Contact
-from django.db.models import Q
-from django.core.paginator import Paginator
-from django import forms
-
-class ContacForm(forms.ModelForm):
-  class Meta:
-    model = Contact
-    fields = (
-      'first_name',
-      'last_name',
-      'phone'
-    )
+from django.shortcuts import render
+from contact.forms import ContacForm
 
 # Create your views here.
 def create(request):
