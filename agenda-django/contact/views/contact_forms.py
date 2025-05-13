cc17e3a6a73cb1a2bf3ae320a69757e8b9a0d7bf
flvsantos15 <flvsantos300@gmail.com>
2025-05-13 15:06:10 -0300

    chore(contact_form): add update view for contact

--- agenda-django/contact/views/contact_forms.py
+++ agenda-django/contact/views/contact_forms.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContacForm
+from django.urls import reverse
+from contact.models import Contact
 
 # Create your views here.
 def create(request):
+  form_action = reverse('contact:create')
+
   if request.method == 'POST':
     form = ContacForm(request.POST)
 
-    
-
     context = {
       'form': form,
+      'form_action': form_action,
     }
 
     if form.is_valid():
-      form.save()
-
-      return redirect('contact:index')
+      contact = form.save()
+      return redirect('contact:update', contact_id=contact.pk)
 
 
     return render(
@@ -26,6 +28,7 @@ def create(request):
 
   context = {
     'form': ContacForm(),
+    'form_action': form_action,
   }
 
   return render(
@@ -34,3 +37,36 @@ def create(request):
     context
   )
 
+def update(request, contact_id):
+  contact = get_object_or_404(Contact, pk=contact_id, show=True)
+  form_action = reverse('contact:update', args=(contact_id,))
+
+  if request.method == 'POST':
+    form = ContacForm(request.POST, instance=contact)
+
+    context = {
+      'form': form,
+      'form_action': form_action,
+    }
+
+    if form.is_valid():
+      contact = form.save()
+      return redirect('contact:update', contact_id=contact.pk)
+
+
+    return render(
+      request,
+      'contact/create.html',
+      context
+    )
+
+  context = {
+    'form': ContacForm(instance=contact),
+    'form_action': form_action,
+  }
+
+  return render(
+    request,
+    'contact/create.html',
+    context
+  )
